67ed53f5baa38e9f45f1ff41421839a2eec36d67
Vinícius R. Miguel <36349314+vrmiguel@users.noreply.github.com>
2019-09-27 00:33:24 -0300

    Add files via upload

---
+++ Paradigmas de Programação/Aula 2/main.hs
@@ -0,0 +1,17 @@
+import Soma
+import Pessoa
+import Valida
+
+main = do
+    print("---Testando módulo Soma ---")
+    let t = (2, 2)
+    soma_teste(t)
+    print ("--------------------------")
+    
+    print(pessoa 2)
+    print(idade (pessoa 2))
+    print(quantHomens(10))
+    print(dadosMenorIdade(5))
+    print("------------")
+    print(valida (29, 02, 2000))
+    print(valida (29, 02, 1999))
